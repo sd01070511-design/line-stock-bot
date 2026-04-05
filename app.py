c33e1365f16ced379aa2a016213ad73a242999b7
sd01070511-design <sd01070511@gmail.com>
2026-04-05 16:13:03 +0800

    Set app to run on specified port from environment

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
